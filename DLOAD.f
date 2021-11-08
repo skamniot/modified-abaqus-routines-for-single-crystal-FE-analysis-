e5c28ec8b4e60d9bc4fca0f62dc8e62f5a6388e3
skamniot <93625630+skamniot@users.noreply.github.com>
2021-11-08 17:27:14 +0000

    Update DLOAD.f

--- DLOAD.f
+++ DLOAD.f
@@ -11,30 +11,82 @@ C
 C
       DIMENSION TIME(2), COORDS (3)
       CHARACTER*80 SNAME
-c
-c     Maximum nominal traction at steady state of cycle
-      REAL, PARAMETER::   p_nom=100
 c      
-      REAL, PARAMETER::   tc=1     
+*************************************************************************
+c     Determine loading type: 
+!     1 = ramp traction   
+!     10 = cyclic traction
+!     2 = ramp traction on double plate model  
+!     20 = cyclic traction on double plate model
+      integer, parameter :: loadtype = 1
+*************************************************************************
+c     Maximum traction 
+      REAL, PARAMETER::   p_nom=150
 c      
-      REAL, PARAMETER:: T_cycle=2, t_up=0.02, t_down=0.02, t_rest=0.5 
-      REAL period, k, t_steady, p_t
-c     Geometric dimensions      
+      REAL, PARAMETER::   tc=1    ! for double plate model 
+c      
+      REAL, PARAMETER::  t_ramp=1    ! for loadtype = 1,2
+c      
+!     for lodtype = 10, 20
+      REAL, PARAMETER::  T_cycle=2, t_up=0.02, t_down=0.02, t_rest=0.5 
+c      
+      REAL period, k, t_steady, p_t, p_net
+c      
+c     Geometric dimensions for double plate model     
       REAL, PARAMETER:: th=1, W=5, D_film=0.6, D_imp=0.6, film_angle=60
-      REAL S_film, S_imp, S_nom, film_angle_rad, p_net
+      REAL S_film, S_imp, S_nom, film_angle_rad
 c
-c     calculate p_net at steady state (maximum)
+c
+c     loading time variables
+        k=INT(TIME(2)/T_cycle)
+        t_steady=T_cycle-t_up-t_down
+        period=T_cycle+t_rest
+        k=INT(TIME(2)/period)
+c
+c     calculate p_net for double plate model
       film_angle_rad=film_angle*pi/180
       S_nom=(tc+th)*W
       S_film=D_film*th/((cos(film_angle_rad))**2)
       S_imp=D_imp*tc
       p_net=p_nom*S_nom/(S_nom-2*S_film-S_imp)
-c         
-      k=INT(TIME(2)/T_cycle)
-      t_steady=T_cycle-t_up-t_down
-        period=T_cycle+t_rest
-        k=INT(TIME(2)/period)
 c
+c
+      SELECT CASE(loadtype)
+c          
+      CASE(1) !ramp traction
+c
+      if (TIME(2) .LE. t_ramp) then
+          p_t=(p_nom/t_ramp)*TIME(2)
+      else
+          p_t=p_nom
+      end if
+      F=-p_t
+c          
+      CASE(10) !cyclic traction
+c
+      if (TIME(2) .LE. k*period + t_up) then
+          p_t=(p_nom/t_up)*(TIME(2)-k*period)
+      else if (TIME(2) .LE. k*period + t_up+t_steady) then
+          p_t=p_nom
+      else if (TIME(2) .LE. k*period + t_up+t_steady+t_down) then
+          p_t=p_nom-(p_nom/t_down)*
+     1        (TIME(2)-k*period-t_up-t_steady)
+      else    
+          p_t=0.0
+      end if
+      F=-p_t
+c          
+      CASE(2) !ramp traction on double plate  
+c
+      if (TIME(2) .LE. t_ramp) then
+          p_t=(p_net/t_ramp)*TIME(2)
+      else
+          p_t=p_net
+      end if
+      F=-p_t
+c    
+      CASE(20) !ramp traction on double plate  
+c 
       if (TIME(2) .LE. k*period + t_up) then
           p_t=(p_net/t_up)*(TIME(2)-k*period)
         else if (TIME(2) .LE. k*period + t_up+t_steady) then
@@ -45,9 +97,12 @@ c
         else    
           p_t=0.0
         end if
+      F=-p_t
 c        
-c   Provide F to ABAQUS
-        F=-p_t
+      case default      
+      WRITE(*,*)"Not sure what type of displacement controlled loading"
+      END SELECT
+c      
 c          
       RETURN
       END
